752a8a9828f61972069c5036c807427f1da787e8
Gary <yunqi_yao@yahoo.com>
2017-02-06 16:40:37 -0500

    Initial upload

---
+++ PNP merger/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace PNP_merger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openExcelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mergeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Reference_des = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Des = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.countText = new System.Windows.Forms.TextBox();
+            this.Count = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.countText2 = new System.Windows.Forms.TextBox();
+            this.Count2 = new System.Windows.Forms.Label();
+            this.count2Btn = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Ref_des = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Z = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tOrB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(878, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openExcelToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openExcelToolStripMenuItem
+            // 
+            this.openExcelToolStripMenuItem.Name = "openExcelToolStripMenuItem";
+            this.openExcelToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.openExcelToolStripMenuItem.Text = "Open Excel BOM";
+            this.openExcelToolStripMenuItem.Click += new System.EventHandler(this.openExcelToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.openToolStripMenuItem.Text = "Open Excel PNP";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mergeToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // mergeToolStripMenuItem
+            // 
+            this.mergeToolStripMenuItem.Enabled = false;
+            this.mergeToolStripMenuItem.Name = "mergeToolStripMenuItem";
+            this.mergeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mergeToolStripMenuItem.Text = "Merge";
+            this.mergeToolStripMenuItem.Click += new System.EventHandler(this.mergeToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Reference_des,
+            this.Des});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(410, 442);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Reference_des
+            // 
+            this.Reference_des.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Reference_des.HeaderText = "Reference designator ";
+            this.Reference_des.Name = "Reference_des";
+            // 
+            // Des
+            // 
+            this.Des.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Des.HeaderText = "Description";
+            this.Des.Name = "Des";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Expand";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // countText
+            // 
+            this.countText.Location = new System.Drawing.Point(380, 454);
+            this.countText.Name = "countText";
+            this.countText.ReadOnly = true;
+            this.countText.Size = new System.Drawing.Size(36, 20);
+            this.countText.TabIndex = 6;
+            // 
+            // Count
+            // 
+            this.Count.AutoSize = true;
+            this.Count.Location = new System.Drawing.Point(339, 459);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(35, 13);
+            this.Count.TabIndex = 7;
+            this.Count.Text = "Count";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Ref_des,
+            this.X,
+            this.Y,
+            this.Z,
+            this.tOrB});
+            this.dataGridView2.Location = new System.Drawing.Point(446, 6);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(389, 442);
+            this.dataGridView2.TabIndex = 8;
+            // 
+            // countText2
+            // 
+            this.countText2.Location = new System.Drawing.Point(799, 454);
+            this.countText2.Name = "countText2";
+            this.countText2.ReadOnly = true;
+            this.countText2.Size = new System.Drawing.Size(36, 20);
+            this.countText2.TabIndex = 9;
+            // 
+            // Count2
+            // 
+            this.Count2.AutoSize = true;
+            this.Count2.Location = new System.Drawing.Point(758, 459);
+            this.Count2.Name = "Count2";
+            this.Count2.Size = new System.Drawing.Size(35, 13);
+            this.Count2.TabIndex = 10;
+            this.Count2.Text = "Count";
+            // 
+            // count2Btn
+            // 
+            this.count2Btn.Location = new System.Drawing.Point(677, 454);
+            this.count2Btn.Name = "count2Btn";
+            this.count2Btn.Size = new System.Drawing.Size(75, 23);
+            this.count2Btn.TabIndex = 11;
+            this.count2Btn.Text = "Count";
+            this.count2Btn.UseVisualStyleBackColor = true;
+            this.count2Btn.Click += new System.EventHandler(this.count2Btn_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(858, 506);
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.countText2);
+            this.tabPage1.Controls.Add(this.Count2);
+            this.tabPage1.Controls.Add(this.count2Btn);
+            this.tabPage1.Controls.Add(this.dataGridView2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.Count);
+            this.tabPage1.Controls.Add(this.countText);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(850, 480);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Merger";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1000, 480);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Checker";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Ref_des
+            // 
+            this.Ref_des.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Ref_des.HeaderText = "Reference designator";
+            this.Ref_des.Name = "Ref_des";
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            this.X.Width = 60;
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            this.Y.Width = 60;
+            // 
+            // Z
+            // 
+            this.Z.HeaderText = "Z";
+            this.Z.Name = "Z";
+            this.Z.Width = 60;
+            // 
+            // tOrB
+            // 
+            this.tOrB.HeaderText = "Top/Bot";
+            this.tOrB.Name = "tOrB";
+            this.tOrB.Width = 60;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 545);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Util";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openExcelToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Reference_des;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Des;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox countText;
+        private System.Windows.Forms.Label Count;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox countText2;
+        private System.Windows.Forms.Label Count2;
+        private System.Windows.Forms.Button count2Btn;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mergeToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ref_des;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Z;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tOrB;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ PNP merger/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Threading;
+using System.Globalization;
+using System.Collections;
+
+namespace PNP_merger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string file;
+        //private int rowIndex = 0;
+        
+        private void openExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.Filter = "Office Files|*.xls;";
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                file = openFileDialog1.FileName;
+                // }
+                Console.WriteLine(result); // <-- For debugging use.
+
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
+                xlWorkbook = xlApp.Workbooks.Open(file);
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                int i = 1;
+                int j = 1;
+                while (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                {    //outputData.AppendText(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                    string[] row = { xlRange.Cells[i, j].Value2.ToString(), xlRange.Cells[i, 2].Value2.ToString() };
+                    dataGridView1.Rows.Add(row);
+                    i++;
+                }
+
+                xlApp.Quit();
+
+                //release all memory - stop EXCEL.exe from hanging around.
+                if (xlWorkbook != null) { Marshal.ReleaseComObject(xlWorkbook); } //release each workbook like this
+                if (xlWorksheet != null) { Marshal.ReleaseComObject(xlWorksheet); } //release each worksheet like this
+                if (xlApp != null) { Marshal.ReleaseComObject(xlApp); } //release the Excel application
+                xlWorkbook = null; //set each memory reference to null.
+                xlWorksheet = null;
+                xlApp = null;
+                GC.Collect();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int[] combined = new int [100]; //Max 100 combined reference desginators.
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            ArrayList list = new ArrayList();//list of all indexes that contains combined reference desginators. 
+            ArrayList partNo = new ArrayList();
+            String type;
+            int count = 0;
+            dataGridView1.Columns[0].Name = "Reference designator";
+            dataGridView1.Columns[1].Name = "Part number";
+            for (int i =0; i<dataGridView1.RowCount && dataGridView1[0, i].Value!=null; i++)
+            {
+                if (((dataGridView1[0, i].Value).ToString()).Contains(','))
+                {
+                    list.Add(i); //Get an array of indexes that containes combined reference desginators.
+                    partNo.Add(dataGridView1[1, i].Value.ToString());
+                }
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                string[] words = dataGridView1[0,Int32.Parse(list[i].ToString())].Value.ToString().Split(delimiterChars);//get the type of reference designators.
+                string[] rowContent=new string[2];
+                if (Char.IsLetter(words[0][1]))//check whethere there are 2 letter for the ref des
+                    type = words[0][0].ToString() + words[0][1].ToString();
+                else
+                    type = words[0][0].ToString();
+                for(int f=0; f < words.Length; f++)//add in the rows at the end
+                {
+                    if (Char.IsLetter(words[f][0]))
+                        ;
+                    else
+                        words[f] = type + words[f];
+                    rowContent[0] = words[f];
+                    rowContent[1] = partNo[i].ToString();
+                    dataGridView1.Rows.Add(rowContent);
+                }             
+            }
+            for(int i=list.Count-1; i>=0; i--)
+            {
+                dataGridView1.Rows.Remove(dataGridView1.Rows[Int32.Parse(list[i].ToString())]);
+            }
+            count = dataGridView1.Rows.Count-1;
+            countText.Text = count.ToString();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.Filter = "Office Files|*.xls;";
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                file = openFileDialog1.FileName;
+                // }
+                Console.WriteLine(result); // <-- For debugging use.
+
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
+                xlWorkbook = xlApp.Workbooks.Open(file);
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                int i = 1;
+                //int j = 1;
+                while (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 1].Value2 != null)
+                {    //outputData.AppendText(xlRange.Cells[i, j].Value2.ToString() + "\t");
+                    string[] row = { xlRange.Cells[i, 1].Value2.ToString(), xlRange.Cells[i, 2].Value2.ToString(), xlRange.Cells[i, 3].Value2.ToString(), xlRange.Cells[i, 4].Value2.ToString(), xlRange.Cells[i, 5].Value2.ToString()};
+                    dataGridView2.Rows.Add(row);
+                    i++;
+                }
+
+                xlApp.Quit();
+
+                //release all memory - stop EXCEL.exe from hanging around.
+                if (xlWorkbook != null) { Marshal.ReleaseComObject(xlWorkbook); } //release each workbook like this
+                if (xlWorksheet != null) { Marshal.ReleaseComObject(xlWorksheet); } //release each worksheet like this
+                if (xlApp != null) { Marshal.ReleaseComObject(xlApp); } //release the Excel application
+                xlWorkbook = null; //set each memory reference to null.
+                xlWorksheet = null;
+                xlApp = null;
+                GC.Collect();
+            }
+        }
+
+        private void count2Btn_Click(object sender, EventArgs e)
+        {
+            int count = dataGridView2.Rows.Count - 1;
+            countText2.Text = count.ToString();
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.Rows.Count!=1 && dataGridView1.Rows.Count!=1)
+            {
+                mergeToolStripMenuItem.Enabled=true;
+            }
+        }
+
+        private void mergeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //ArrayList componentMatch = new ArrayList();
+            ArrayList componentTop = new ArrayList();
+            ArrayList componentBot = new ArrayList();
+            ArrayList componentMissing = new ArrayList();
+            Boolean componentFound = false;
+            //string[] componentDetails = new string[6];
+            for (int i = 0; i < dataGridView1.RowCount && dataGridView1[0, i].Value != null; i++)
+            {
+                if (componentFound)
+                {
+                    componentMissing.Add((dataGridView1[0, i].Value).ToString());
+                }
+                componentFound = false;
+
+                for (int j = 0; j < dataGridView2.RowCount && dataGridView2[0, j].Value != null; j++)
+                {
+                    if (((dataGridView1[0, i].Value).ToString()) == ((dataGridView2[0, j].Value).ToString())) // if a component in the BOM matches a component in the PNP file
+                    {
+                        string[] componentDetails = new string[6];
+                        componentDetails[0] = (dataGridView1[0, i].Value).ToString(); //ref des
+                        componentDetails[1] = (dataGridView2[1, j].Value).ToString(); //x
+                        componentDetails[2] = (dataGridView2[2, j].Value).ToString();//y
+                        componentDetails[3] = (dataGridView2[3, j].Value).ToString();//z
+                        componentDetails[4] = (dataGridView2[4, j].Value).ToString();//top or bot
+                        componentDetails[5] = (dataGridView1[1, i].Value).ToString();//description
+                        if (componentDetails[4][0].Equals('t') || componentDetails[4][0].Equals('T'))
+                        {
+                            componentTop.Add(componentDetails);
+                        }
+                        if (componentDetails[4][0].Equals('b') || componentDetails[4][0].Equals('B'))
+                        {
+                            componentBot.Add(componentDetails);
+                        }
+                        componentFound = true;
+                    }
+                }
+
+                //if (componentNotFound)
+                //{
+                //    componentDetails[0] = (dataGridView1[0, i].Value).ToString(); //ref des
+                //    componentMissing.Add(componentDetails);
+                //}
+            }
+
+        }
+    }
+}
